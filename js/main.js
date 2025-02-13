044871d0e59ac53b191cd75d53f44478ce81a910
Mehn321 <aclonhemday@gmail.com>
2025-02-13 23:53:11 +0800

    add

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Valentines Day 💓').split('')
+    const titles = ('Happy Valentines Day MyLove 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
